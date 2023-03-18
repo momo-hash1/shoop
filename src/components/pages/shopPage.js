188ca91c58ea9d778bc75bc9fef6a2acddc64f06
momo934 <ask4r.safiullin@yandex.ru>
2023-03-18 08:20:15 +0300

    paginator

--- src/components/pages/shopPage.js
+++ src/components/pages/shopPage.js
@@ -3,12 +3,14 @@ import React from "react";
 import ShopItemList from "../shopItem/shopItemList";
 import WithAside from "../withAside";
 import GoToShoppingCart from "../nav/goToShoppingCart";
+import Paginator from "../paginator";
 
 const ShopPage = () => {
   return (
     <React.Fragment>
       <WithAside aside={<Filter />}>
         <ShopItemList />
+        <Paginator/>
       </WithAside>
       <GoToShoppingCart />
     </React.Fragment>
---
+++ src/components/paginator.js
@@ -0,0 +1,21 @@
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const Paginator = (props) => {
+  return (
+    <Pagination
+      sx={{ marginTop: 2, marginBottom: 2 }}
+      count={10}
+      renderItem={(item) => (
+        <PaginationItem
+          slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+          {...item}
+        />
+      )}
+    />
+  );
+};
+
+export default Paginator;
